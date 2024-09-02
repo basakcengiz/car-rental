ee4321e8e650b685aa2c6f29b443e7e48f885a76
Başak Cengiz <120058178+basakcengiz@users.noreply.github.com>
2024-09-02 17:10:39 +0300

    date picker updated

--- src/components/BookCar/BookCar.tsx
+++ src/components/BookCar/BookCar.tsx
@@ -7,88 +7,88 @@ import DatePickerFC from '../inputComponents/DatePicker';
 import { Button, InputLabel, MenuItem, Select } from '@mui/material';
 
 const BookCar = () => {
-  const [date, setDate] = useState<string | null>('2024-09-01T15:30');
-  const { t, i18n } = useTranslation();
-  const [office, setOffice] = useState('Istanbul Ofis');
+   const [date, setDate] = useState<string | null>('2024-09-01T15:30');
+   const { t, i18n } = useTranslation();
+   const [office, setOffice] = useState('Istanbul Ofis');
 
-  return (
-    <Grid id="book" container justifyContent={'center'} alignItems={'center'} flexDirection={'column'}>
-      <div className='bg-[url("/src/assets/background.png")] h-max w-full border border-spacing-1 flex '>
-        <Grid container size={{ xs: 12 }} justifyContent={'center'}>
-          <Grid container className="p-10" flexDirection={'column'} spacing={1}>
-            <InputLabel>{t('pickUpOffice')}</InputLabel>
-            <Select
-              value={office}
-              defaultValue={office}
-              onChange={(e) => {
-                setOffice(e.target.value);
-              }}
-              /*   MenuProps={{
+   return (
+      <Grid id="book" container justifyContent={'center'} alignItems={'center'} flexDirection={'column'}>
+         <div className='bg-[url("/src/assets/background.png")] h-max w-full border border-spacing-1 flex '>
+            <Grid container size={{ xs: 12 }} justifyContent={'center'}>
+               <Grid container className="p-10" flexDirection={'column'} spacing={1}>
+                  <InputLabel>{t('pickUpOffice')}</InputLabel>
+                  <Select
+                     value={office}
+                     defaultValue={office}
+                     onChange={(e) => {
+                        setOffice(e.target.value);
+                     }}
+                     /*   MenuProps={{
               sx: {
                 fontSize: '8px'
               }
             }} */
-              sx={{
-                height: '40px',
-                width: '250px',
-                fontSize: '14px',
-                '& svg': {
-                  color: '#FF7F3E'
-                },
-                '.MuiOutlinedInput-notchedOutline': { border: 1, borderColor: '#FF7F3E' },
-                '&.MuiOutlinedInput-root.Mui-focused': {
-                  '.MuiOutlinedInput-notchedOutline': { border: 1, borderColor: '#FF7F3E' }
-                }
-              }}
-            >
-              <MenuItem value="Istanbul Ofis">Istanbul Havalimanı Ofis</MenuItem>
-              <MenuItem value="Ankara Ofis">Ankara ofis</MenuItem>
-              <MenuItem value="Ant Ofis">Ant Ofis</MenuItem>
-              <MenuItem value="Izmır Ofis">Izmır Ofis</MenuItem>
-              <MenuItem value="Sabiha Ofis">Sabiha</MenuItem>
-            </Select>
-            <InputLabel>{t('dropOffOffice')}</InputLabel>
-            <Select
-              value={office}
-              defaultValue={office}
-              onChange={(e) => {
-                setOffice(e.target.value);
-              }}
-              sx={{
-                height: '40px',
-                width: '250px',
-                fontSize: '14px',
-                '& svg': {
-                  color: '#FF7F3E'
-                },
-                '.MuiOutlinedInput-notchedOutline': { border: 1, borderColor: '#FF7F3E' },
-                '&.MuiOutlinedInput-root.Mui-focused': {
-                  '.MuiOutlinedInput-notchedOutline': { border: 1, borderColor: '#FF7F3E' }
-                }
-              }}
-            >
-              <MenuItem value="Istanbul Ofis">Istanbul Ofis</MenuItem>
-              <MenuItem value="Ankara Ofis">Ankara ofis</MenuItem>
-              <MenuItem value="Ant Ofis">Ant Ofis</MenuItem>
-              <MenuItem value="Izmır Ofis">Izmır Ofis</MenuItem>
-              <MenuItem value="Sabiha Ofis">Sabiha</MenuItem>
-            </Select>
-          </Grid>
-          <Grid container className="p-10" flexDirection={'column'} spacing={1}>
-            <InputLabel>{t('pickUpDate')}</InputLabel>
-            <DatePickerFC date={date} setDate={setDate} disablePast />
-            <InputLabel>{t('dropOffDate')}</InputLabel>
-            <DatePickerFC date={date} setDate={setDate} disablePast />
-          </Grid>
-        </Grid>
-      </div>
-      <Grid container className="p-10" justifyContent={'center'} alignItems={'center'}>
-        <Button variant="contained" sx={{ color: '#0C2D57', backgroundColor: '#FF7F3E', width: '150px' }} className="rounded-md">
-          {t('bookCar').toLocaleUpperCase(i18n.language)}
-        </Button>
+                     sx={{
+                        height: '40px',
+                        width: '250px',
+                        fontSize: '14px',
+                        '& svg': {
+                           color: '#FF7F3E',
+                        },
+                        '.MuiOutlinedInput-notchedOutline': { border: 1, borderColor: '#FF7F3E' },
+                        '&.MuiOutlinedInput-root.Mui-focused': {
+                           '.MuiOutlinedInput-notchedOutline': { border: 1, borderColor: '#FF7F3E' },
+                        },
+                     }}
+                  >
+                     <MenuItem value="Istanbul Ofis">Istanbul Havalimanı Ofis</MenuItem>
+                     <MenuItem value="Ankara Ofis">Ankara ofis</MenuItem>
+                     <MenuItem value="Ant Ofis">Ant Ofis</MenuItem>
+                     <MenuItem value="Izmır Ofis">Izmır Ofis</MenuItem>
+                     <MenuItem value="Sabiha Ofis">Sabiha</MenuItem>
+                  </Select>
+                  <InputLabel>{t('dropOffOffice')}</InputLabel>
+                  <Select
+                     value={office}
+                     defaultValue={office}
+                     onChange={(e) => {
+                        setOffice(e.target.value);
+                     }}
+                     sx={{
+                        height: '40px',
+                        width: '250px',
+                        fontSize: '14px',
+                        '& svg': {
+                           color: '#FF7F3E',
+                        },
+                        '.MuiOutlinedInput-notchedOutline': { border: 1, borderColor: '#FF7F3E' },
+                        '&.MuiOutlinedInput-root.Mui-focused': {
+                           '.MuiOutlinedInput-notchedOutline': { border: 1, borderColor: '#FF7F3E' },
+                        },
+                     }}
+                  >
+                     <MenuItem value="Istanbul Ofis">Istanbul Ofis</MenuItem>
+                     <MenuItem value="Ankara Ofis">Ankara ofis</MenuItem>
+                     <MenuItem value="Ant Ofis">Ant Ofis</MenuItem>
+                     <MenuItem value="Izmır Ofis">Izmır Ofis</MenuItem>
+                     <MenuItem value="Sabiha Ofis">Sabiha</MenuItem>
+                  </Select>
+               </Grid>
+               <Grid container className="p-10" flexDirection={'column'} spacing={1}>
+                  <InputLabel>{t('pickUpDate')}</InputLabel>
+                  <DatePickerFC date={date} setDate={setDate} disablePast clearable />
+                  <InputLabel>{t('dropOffDate')}</InputLabel>
+                  <DatePickerFC date={date} setDate={setDate} disablePast clearable />
+               </Grid>
+            </Grid>
+         </div>
+         <Grid container className="p-10" justifyContent={'center'} alignItems={'center'}>
+            <Button variant="contained" sx={{ color: '#0C2D57', backgroundColor: '#FF7F3E', width: '150px' }} className="rounded-md">
+               {t('bookCar').toLocaleUpperCase(i18n.language)}
+            </Button>
+         </Grid>
       </Grid>
-    </Grid>
-  );
+   );
 };
 
 export default BookCar;
--- src/components/inputComponents/DatePicker.tsx
+++ src/components/inputComponents/DatePicker.tsx
@@ -1,4 +1,4 @@
-import { DesktopDatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 /* import CalendarMonthIcon from '@mui/icons-material/CalendarMonth'; */
 import { useTranslation } from 'react-i18next';
 import { useState } from 'react';
@@ -7,89 +7,107 @@ import 'dayjs/locale/tr';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import 'dayjs/locale/tr';
 import { trTR, enUS } from '@mui/x-date-pickers/locales';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 
 type IProps = {
-  placeholder?: string;
-  setDate: (date: string | null) => void;
-  testId?: string;
-  date: string | null;
-  clearable?: boolean;
-  error?: boolean;
-  /*   setTime?: React.Dispatch<React.SetStateAction<string | null>>; */
-  disabled?: boolean;
-  disablePast?: boolean;
-  sx?: Object;
-  maxDate?: dayjs.Dayjs;
+   placeholder?: string;
+   setDate: (date: string | null) => void;
+   testId?: string;
+   date: string | null;
+   clearable?: boolean;
+   error?: boolean;
+   /*   setTime?: React.Dispatch<React.SetStateAction<string | null>>; */
+   disabled?: boolean;
+   disablePast?: boolean;
+   sx?: Object;
+   //object
+   maxDate?: dayjs.Dayjs;
 };
 
 export default function DatePickerFC(props: IProps) {
-  const { t, i18n } = useTranslation();
+   const { t, i18n } = useTranslation();
 
-  const [pickerOpen, setPickerOpen] = useState(false);
-  const turkishLocale = trTR.components.MuiLocalizationProvider.defaultProps.localeText;
-  const englishLocale = enUS.components.MuiLocalizationProvider.defaultProps.localeText;
+   const [pickerOpen, setPickerOpen] = useState(false);
+   const turkishLocale = trTR.components.MuiLocalizationProvider.defaultProps.localeText;
+   const englishLocale = enUS.components.MuiLocalizationProvider.defaultProps.localeText;
 
-  return (
-    <LocalizationProvider
-      dateAdapter={AdapterDayjs}
-      adapterLocale={i18n.language === 'tr' ? 'tr' : 'en'}
-      localeText={i18n.language === 'tr' ? turkishLocale : englishLocale}
-    >
-      <div data-testid={props.testId + 'ParentDiv'}>
-        <DesktopDatePicker
-          value={props.date ? dayjs(props.date, 'YYYY-MM-DD') : null}
-          data-testid={props.testId}
-          onChange={(date: dayjs.Dayjs | null) => {
-            let formattedDate = date?.format('YYYY-MM-DD');
-            props.setDate(formattedDate || null);
-            /*        props.setTime && props.setTime(null); */
-          }}
-          disablePast={props.disablePast ?? true}
-          disabled={props.disabled ? props.disabled : false}
-          format={i18n.language === 'tr' ? 'DD/MMM/YYYY' : 'MMM/DD/YYYY'}
-          maxDate={props?.maxDate}
-          open={pickerOpen}
-          onClose={() => setPickerOpen(false)}
-          sx={
-            props.sx
-              ? props.sx
-              : {
-                  '& .MuiInputBase-root': {
-                    fontWeight: 400,
-                    fontSize: '14px',
-                    backgroundColor: '#f6f8f9'
+   return (
+      <LocalizationProvider
+         dateAdapter={AdapterDayjs}
+         adapterLocale={i18n.language === 'tr' ? 'tr' : 'en'}
+         localeText={i18n.language === 'tr' ? turkishLocale : englishLocale}
+      >
+         <div data-testid={props.testId + 'ParentDiv'}>
+            <DatePicker
+               value={props.date ? dayjs(props.date, 'YYYY-MM-DD') : null}
+               data-testid={props.testId}
+               onChange={(date: dayjs.Dayjs | null) => {
+                  //const
+                  let formattedDate = date?.format('YYYY-MM-DD');
+                  props.setDate(formattedDate || null);
+                  /*        props.setTime && props.setTime(null); */
+               }}
+               disablePast={props.disablePast ?? true}
+               disabled={props.disabled ? props.disabled : false}
+               format={i18n.language === 'tr' ? 'DD/MMM/YYYY' : 'MMM/DD/YYYY'}
+               maxDate={props?.maxDate}
+               open={pickerOpen}
+               onClose={() => setPickerOpen(false)}
+               sx={
+                  props.sx
+                     ? props.sx
+                     : {
+                          '& .MuiInputBase-root': {
+                             fontWeight: 400,
+                             fontSize: '14px',
+                             backgroundColor: '#f6f8f9',
+                          },
+                          '& .MuiOutlinedInput-root': {
+                             height: '40px',
+                             fontSize: '14px',
+                             width: '250px',
+                             '&:hover fieldset': {
+                                borderColor: '#FF7F3E',
+                             },
+                             '&.Mui-focused fieldset': {
+                                borderColor: '#FF7F3E',
+                             },
+                          },
+                       }
+               }
+               slotProps={{
+                  openPickerButton: {
+                     onClick: () => setPickerOpen(true),
                   },
-                  '& .MuiOutlinedInput-root': {
-                    height: '40px',
-                    fontSize: '14px',
-                    width: '250px',
-                    '&:hover fieldset': {
-                      borderColor: '#FF7F3E'
-                    },
-                    '&.Mui-focused fieldset': {
-                      borderColor: '#FF7F3E'
-                    }
-                  }
-                }
-          }
-          slotProps={{
-            textField: {
-              error: props.error,
-              onClick: () => setPickerOpen(true)
-              /*  inputProps: {
-                readOnly: true
-              }, */
-              /*       placeholder: props.placeholder ? props.placeholder : t('date') */
-            },
-            actionBar: {
-              actions: [props.clearable ? 'clear' : 'cancel', 'accept']
-            }
-          }}
-          /*  slots={{
-            openPickerIcon: CalendarMonthIcon
-          }} */
-        />
-      </div>
-    </LocalizationProvider>
-  );
+                  textField: {
+                     error: props.error,
+                     onClick: () => setPickerOpen(true),
+                     placeholder: props.placeholder ? props.placeholder : t('pickDate'),
+                     /*    sx: { backgroundColor: '#FF7F3E' }, */
+                     /*  inputProps: {
+                        readOnly: true,
+                     }, */
+                  },
+                  actionBar: {
+                     actions: [props.clearable ? 'clear' : 'cancel', 'accept'],
+                     sx: {
+                        '.MuiButtonBase-root': {
+                           backgroundColor: '#FF7F3E',
+                           color: '#FFFFFF',
+                        },
+                        //todo: give background color to selected date
+                        /*  '.MuiButtonBase-root.MuiPickersDay-root.Mui-selected': {
+                           backgroundColor: '#FF7F3E',
+                           color: '#FF7F3E',
+                        }, */
+                     },
+                  },
+               }}
+               slots={{
+                  openPickerIcon: CalendarMonthIcon,
+               }}
+            />
+         </div>
+      </LocalizationProvider>
+   );
 }
