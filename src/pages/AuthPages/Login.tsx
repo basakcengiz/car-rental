1c6e41bf9469f6ba0f3633863cac98ecbab3c66e
Başak Cengiz <120058178+basakcengiz@users.noreply.github.com>
2024-09-04 13:52:26 +0300

    login page updated

--- src/pages/AuthPages/Login.tsx
+++ src/pages/AuthPages/Login.tsx
@@ -1,93 +1,100 @@
-import { Button, IconButton, InputAdornment, InputLabel, OutlinedInput, Paper, TextField } from '@mui/material';
+import { Button, FormHelperText, IconButton, InputAdornment, InputLabel, OutlinedInput, Paper, Stack, TextField } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 import { useFormik } from 'formik';
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import validationSchema from '../../validationSchemas/loginSchema';
 import { useTranslation } from 'react-i18next';
 import { VisibilityOff, Visibility } from '@mui/icons-material';
+import ForgotPassword from './ForgotPassword';
 
 const Login = () => {
-  const navigate = useNavigate();
-  const { t, i18n } = useTranslation();
-  const [initialValues] = useState({} as any);
+   const navigate = useNavigate();
+   const { t, i18n } = useTranslation();
+   const [initialValues] = useState({} as any);
 
-  const [showPassword, setShowPassword] = useState(false);
+   const [showPassword, setShowPassword] = useState(false);
 
-  const handleClickShowPassword = () => setShowPassword((show) => !show);
+   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
-  const formik = useFormik({
-    initialValues,
-    validationSchema: validationSchema,
-    onSubmit: (values) => {
-      console.log('values', values);
-      setSubmitting(false);
-    }
-  });
+   const formik = useFormik({
+      initialValues,
+      validationSchema: validationSchema,
+      onSubmit: (values) => {
+         console.log('values', values);
+         setSubmitting(false);
+      },
+   });
 
-  const { errors, touched, handleSubmit, isSubmitting, isValid, values, handleBlur, handleChange, setSubmitting, resetForm, setFieldValue } = formik;
-  return (
-    <div className="bg-[#F7F8F9] flex justify-center h-screen items-center">
-      <Paper elevation={3} className="w-1/2 h-1/2">
-        <form onSubmit={handleSubmit} className="h-full">
-          <Grid container display={'flex'} flexDirection={'column'} alignItems={'center'} justifyContent={'center'} style={{ height: '100%' }} spacing={4}>
-            <Grid>
-              <InputLabel /* size="small" */>{t('email')}</InputLabel>
-              <TextField
-                fullWidth
-                size="small"
-                name="email"
-                value={values.email}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={touched.email && Boolean(errors.email)}
-                helperText={touched.email && errors.email ? String(errors.email) : null}
-              />
-            </Grid>
-            <Grid>
-              <InputLabel>{t('password')}</InputLabel>
+   const {
+      errors,
+      touched,
+      handleSubmit,
+      isSubmitting,
+      isValid,
+      values,
+      handleBlur,
+      handleChange,
+      setSubmitting,
+      resetForm,
+      setFieldValue,
+      handleMouseDownPassword,
+      handleMouseUpPassword,
+   } = formik;
+   return (
+      <div className="bg-[#F7F8F9] flex justify-center h-screen items-center">
+         <Paper elevation={3} className="w-1/2 h-1/2">
+            <form onSubmit={handleSubmit} className="">
+               <Grid container style={{ width: '100%' }} sx={{ alignItems: 'center', justifyContent: 'center', height: '350px' }}>
+                  <Grid className="w-2/3">
+                     <InputLabel /* size="small" */>{t('email')}</InputLabel>
+                     <OutlinedInput
+                        /*  sx={{ width: '70%' }} */
+                        fullWidth
+                        size="small"
+                        name="email"
+                        value={values.email}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        error={touched.email && Boolean(errors.email)}
+                     />
+                     {touched.email && errors.email && <FormHelperText error>{String(errors.email)}</FormHelperText>}
+                  </Grid>
+                  <Grid className="w-2/3">
+                     <InputLabel>{t('password')}</InputLabel>
 
-              <TextField
-                type={showPassword ? 'text' : 'password'}
-                value={values.password}
-                size="small"
-                name="password"
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      onClick={handleClickShowPassword}
-                      /*    onMouseDown={handleMouseDownPassword}
-                  onMouseUp={handleMouseUpPassword} */
-                      edge="end"
-                    >
-                      {showPassword ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-              ></TextField>
+                     <OutlinedInput
+                        fullWidth
+                        type={showPassword ? 'text' : 'password'}
+                        value={values.password}
+                        size="small"
+                        name="password"
+                        onBlur={handleBlur}
+                        endAdornment={
+                           <InputAdornment position="end">
+                              <IconButton onClick={handleClickShowPassword} edge="end">
+                                 {showPassword ? <Visibility /> : <VisibilityOff />}
+                              </IconButton>
+                           </InputAdornment>
+                        }
+                     />
+                     {touched.password && errors.password && <FormHelperText error>{String(errors.password)}</FormHelperText>}
+                  </Grid>
+                  <Grid className="w-2/3">
+                     <Link to={'/forgot-password'}>{t('forgotPassword')}</Link>
+                  </Grid>
 
-              {/*   <TextField
-                size="small"
-                name="password"
-                value={values.password}
-                onChange={handleChange}
-                 onBlur={handleBlur}
-                  error={touched.email && Boolean(errors.email)}
-                  helperText={touched.email && errors.email ? String(errors.email) : null} 
-              /> */}
-            </Grid>
-
-            <Grid>
-              <Button variant="contained" sx={{ backgroundColor: '#7E9FAF', width: '150px' }} className="rounded-md">
-                {t('login').toLocaleUpperCase(i18n.language)}
-              </Button>
-            </Grid>
-          </Grid>
-        </form>
-      </Paper>
-      {/*  <button onClick={() => navigate('/forgot-password')}>sfsadf</button> */}
-    </div>
-  );
+                  <Grid>
+                     <Button variant="contained" sx={{ backgroundColor: '#7E9FAF', width: '150px' }} className="rounded-md">
+                        {t('login').toLocaleUpperCase(i18n.language)}
+                     </Button>
+                  </Grid>
+               </Grid>
+            </form>
+         </Paper>
+         {/*  <button onClick={() => navigate('/forgot-password')}>sfsadf</button> */}
+      </div>
+   );
 };
 
 export default Login;
